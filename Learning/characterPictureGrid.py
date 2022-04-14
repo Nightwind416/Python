8b7d3132253cb17b72581e200163384c2e2f7e26
Nightwind416 <christopher.e.lorr@gmail.com>
2022-04-13 23:08:06 -0500

    Grid rotate and coin flip streak
    Coin flip streak may need another loook

--- Learning/characterPictureGrid.py
+++ Learning/characterPictureGrid.py
@@ -10,4 +10,32 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
+#   Print grid
+x = 0
+y = 0
+print('Grid')
+print('')
+while x < 9:
+    while y < 6:
+        print(grid[x][y], end="")
+        y += 1
+    x += 1
+    y = 0
+    print('')
+
     
+#   Print rotated grid
+x = 0
+y = 0
+print('')
+print('')
+print('Rotated')
+print('')
+
+while y < 6:
+    while x < 9:
+        print(grid[x][y], end="")
+        x += 1
+    y += 1
+    x = 0
+    print('')
--- Learning/coinFlipStreak.py
+++ Learning/coinFlipStreak.py
@@ -2,20 +2,53 @@
 
 import random
 
-numberOfStreaks = 0
+listStreak = 0
+totalStreaks = 0
 
-for experimentNumber in range(2):
-    toss = []
+for experimentNumber in range(10000):
+    print('Experiment #: ' + str(experimentNumber))
+    flipList = []
     count = 0
     # Code that creates a list of 100 'heads' or 'tails' values.
     for x in range(100):
         flip = random.choice(['Heads', 'Tails'])
 #         print(flip)
-        toss.append(flip)
+        flipList.append(flip)
+    else:
+#         print('Flip list = ' + str(flipList))
     # Code that checks if there is a streak of 6 heads or tails in a row.
+        previous = 'none'
+    for x in flipList:
+        if previous == 'none':
+            print('Initial check')
+        elif x == previous:
+            count += 1
+#             print('Current streak count = ' + str(count))
+        else:
+            if count >= 6:
+                listStreak += 1
+                count = 0
+                print('Experiment # ' + str(experimentNumber) + ' has a streak!')
+            else:
+                count = 0
+#                 print('Experiment # ' + str(experimentNumber) + ' does not have a streak!')
+        previous = x
+    if listStreak >= 1:
+        totalStreaks += 1
+        listStreak = 0
+        print('Adding streak count')
+    else:
+        print('Keeping streak count')
+        continue
+# Print results
+#     print('Experiment #: ' + str(experimentNumber))
+#     print('Flip list = ' + str(flipList))
+    print('Total Streaks = ' + str(totalStreaks))
+    print('')
     
     
     
     
-# print(toss)
-print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+    
+
+print('Chance of streak: %s%%' % (totalStreaks / 100))
