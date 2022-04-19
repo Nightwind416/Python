f27d501e99e14f7f47ee987728e380cc6eea7e29
Nightwind416 <christopher.e.lorr@gmail.com>
2022-04-19 07:36:25 -0500

    switch to visual studio code

--- Learning/rpslsGame.py
+++ Learning/rpslsGame.py
@@ -1,23 +1,24 @@
 # Rock, Paper, Scissors, Lizard, Spock
 
-import random, sys
+import random
+import sys
 
 print('Rock, Paper, Scissors, Lizard, Spock')
 
 # These variables keep track of the number of wins, losses, and ties.
-wins = 0
-losses = 0
-ties = 0
+win_Total = 0
+loss_Total = 0
+tie_Total = 0
 
-while True: # The main game loop.
-    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
-    while True: # The player input loop.
+while True:  # The main game loop.
+    print('%s Wins, %s Losses, %s Ties' % (win_Total, loss_Total, tie_Total))
+    while True:  # The player input loop.
         print('Enter your move: rock, paper, scissors, lizard, spock, or quit')
         playerMove = input()
         if playerMove == 'quit':
-            sys.exit() # Quit the program.
-        if playerMove == 'rock' or playerMove == 'paper' or playerMove == 'scissors'or playerMove == 'lizard'or playerMove == 'spock':
-            break # Break out of the player input loop.
+            sys.exit()  # Quit the program.
+        if playerMove == 'rock' or playerMove == 'paper' or playerMove == 'scissors' or playerMove == 'lizard' or playerMove == 'spock':
+            break  # Break out of the player input loop.
         print('Type one of rock, paper, scissors, lizard, spock, or quit.')
 
     # Display what the player chose:
@@ -53,39 +54,39 @@ while True: # The main game loop.
     # Display and record the win/loss/tie:
     if playerMove == computerMove:
         print('It is a tie!')
-        ties = ties + 1
-    # Checks whecn player chooses rock    
+        tie_Total = tie_Total + 1
+    # Checks when player chooses rock
     elif playerMove == 'rock' and computerMove == 'scissors' or computerMove == 'lizard':
         print('You win!')
-        wins = wins + 1
+        win_Total = win_Total + 1
     elif playerMove == 'rock' and computerMove == 'paper' or computerMove == 'spock':
         print('You lose!')
-        losses = losses + 1
+        loss_Total = loss_Total + 1
     # Checks whecn player chooses paper
     elif playerMove == 'paper' and computerMove == 'rock' or computerMove == 'spock':
         print('You win!')
-        wins = wins + 1
+        win_Total = win_Total + 1
     elif playerMove == 'paper' and computerMove == 'scissors' or computerMove == 'lizard':
         print('You lose!')
-        losses = losses + 1
+        loss_Total = loss_Total + 1
     # Checks whecn player chooses scissors
     elif playerMove == 'scissors' and computerMove == 'paper' or computerMove == 'lizard':
         print('You win!')
-        wins = wins + 1
+        win_Total = win_Total + 1
     elif playerMove == 'scissors' and computerMove == 'rock' or computerMove == 'spock':
         print('You lose!')
-        losses = losses + 1
+        loss_Total = loss_Total + 1
     # Checks whecn player chooses lizard
     elif playerMove == 'lizard' and computerMove == 'paper' or computerMove == 'spock':
         print('You win!')
-        wins = wins + 1
+        win_Total = win_Total + 1
     elif playerMove == 'lizard' and computerMove == 'rock' or computerMove == 'scissors':
         print('You lose!')
-        losses = losses + 1
+        loss_Total = loss_Total + 1
     # Checks whecn player chooses spock
     elif playerMove == 'spock' and computerMove == 'scissors' or computerMove == 'rock':
         print('You win!')
-        wins = wins + 1
+        win_Total = win_Total + 1
     elif playerMove == 'spock' and computerMove == 'paper' or computerMove == 'lizard':
         print('You lose!')
-        losses = losses + 1
+        loss_Total = loss_Total + 1
